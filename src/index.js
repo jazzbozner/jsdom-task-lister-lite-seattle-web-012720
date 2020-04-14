17859c1b4f783537e0119b0230178ab61ed9b26b
github username <github email address>
2020-04-13 17:37:43 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addTask();
 });
+
+function createTask(e) {
+  const taskList = document.getElementById('tasks')
+  let button = document.createElement('button')
+  let task = document.createElement('li')
+  button.innerHTML = 'x'
+  task.innerHTML = e
+  task.appendChild(button)
+  taskList.appendChild(task)
+}
+
+function addTask() {
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    createTask(e.target['new-task-description'].value);
+    e.target.reset();
+  })
+}
+
+function deletTask() {
+  let button = document.querySelector('button')
+  button.addEventListener('click', function(e) {
+    let list = document.getElementById('tasks')
+    // remove child
+  })
+}
+
+
+
+
+// function addTask(task) {
+//   let newTask = document.createElement('li');
+//   newTask.textContent = task;
+//   document.querySelector('ul').appendChild(newTask);
+// }
+
+//   function deleteTask() {
+//   let deleteTask = document.createElement('button')
+//   deleteTask.textContent = "x"
+ 
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+//   const form = document.getElementById('create-task-form');
+  
+//   form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     const taskToAdd = event.target["new-task-description"].value;
+//     if (taskToAdd !== "") {
+//       addTask(taskToAdd);
+//     }
+//   form.addEventListener('button', function(event) {
+//     let elem = document.getElementById('create-task-form')
+//     elem.parentNode.removeChild(elem)
+//     }
+//   });
+// });
