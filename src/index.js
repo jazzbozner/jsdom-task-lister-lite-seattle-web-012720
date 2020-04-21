f056b161de496de83d9a9a27f845778e6955d01d
github username <github email address>
2020-04-21 15:42:54 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,63 +1,39 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+  addPriorities();
   addTask();
 });
+const form = document.getElementById('create-task-form')
 
-function createTask(e) {
-  const taskList = document.getElementById('tasks')
-  let button = document.createElement('button')
-  let task = document.createElement('li')
-  button.innerHTML = 'x'
-  task.innerHTML = e
-  task.appendChild(button)
-  taskList.appendChild(task)
+function addPriorities() {
+  const dropDown = document.createElement('select')
+  dropDown.id = "priority-value"
+  dropDown.innerHTML = "<option value='high'>High</option><option value='medium'>Medium</option><option value='low'>Low</option"
+  form.insertBefore(dropDown, form.childNodes[4])
 }
 
 function addTask() {
-  const form = document.getElementById('create-task-form')
   form.addEventListener('submit', function(e) {
     e.preventDefault();
-    createTask(e.target['new-task-description'].value);
-    e.target.reset();
+    const taskList = document.getElementById('tasks')
+
+    let task = document.createElement('li')
+    task.innerText = e.target['new-task-description'].value
+    task.className = e.target["priority-value"].value
+
+    let button = document.createElement('button')
+    button.innerText = 'x'
+
+    task.appendChild(button)
+    taskList.appendChild(task)
+
+    deleteTask(button);
+      e.target.reset();
   })
 }
 
-function deletTask() {
-  let button = document.querySelector('button')
+function deleteTask(button) {
   button.addEventListener('click', function(e) {
-    let list = document.getElementById('tasks')
-    // remove child
+    button.parentElement.remove();
   })
 }
 
-
-
-
-// function addTask(task) {
-//   let newTask = document.createElement('li');
-//   newTask.textContent = task;
-//   document.querySelector('ul').appendChild(newTask);
-// }
-
-//   function deleteTask() {
-//   let deleteTask = document.createElement('button')
-//   deleteTask.textContent = "x"
- 
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-
-//   const form = document.getElementById('create-task-form');
-  
-//   form.addEventListener("submit", function(event) {
-//     event.preventDefault();
-//     const taskToAdd = event.target["new-task-description"].value;
-//     if (taskToAdd !== "") {
-//       addTask(taskToAdd);
-//     }
-//   form.addEventListener('button', function(event) {
-//     let elem = document.getElementById('create-task-form')
-//     elem.parentNode.removeChild(elem)
-//     }
-//   });
-// });
